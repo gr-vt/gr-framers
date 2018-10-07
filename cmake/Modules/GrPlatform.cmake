9dd8a0f8b18e532a676f0c4261d7fc9bffc0f04e
Igor Freire <igorfreire@ufpa.br>
2018-10-07 17:28:40 +0100

    Update cmake build environment
    Update the build tools and configurations to follow what a more recent
    gr_modtool would produce for a GR OOT.

--- cmake/Modules/GrPlatform.cmake
+++ cmake/Modules/GrPlatform.cmake
@@ -37,6 +37,21 @@ if(NOT CMAKE_CROSSCOMPILING AND LINUX AND EXISTS "/etc/redhat-release")
     set(REDHAT TRUE)
 endif()
 
+if(NOT CMAKE_CROSSCOMPILING AND LINUX AND EXISTS "/etc/slackware-version")
+    set(SLACKWARE TRUE)
+endif()
+
+########################################################################
+# when the library suffix should be 64 (applies to redhat linux family)
+########################################################################
+if (REDHAT OR SLACKWARE)
+    set(LIB64_CONVENTION TRUE)
+endif()
+
+if(NOT DEFINED LIB_SUFFIX AND LIB64_CONVENTION AND CMAKE_SYSTEM_PROCESSOR MATCHES "64$")
+    set(LIB_SUFFIX 64)
+endif()
+
 ########################################################################
 # Detect /lib versus /lib64
 ########################################################################
@@ -44,10 +59,4 @@ if (CMAKE_INSTALL_LIBDIR MATCHES lib64)
     set(LIB_SUFFIX 64)
 endif()
 
-########################################################################
-# when the library suffix should be 64 (applies to redhat linux family)
-########################################################################
-if(NOT DEFINED LIB_SUFFIX AND REDHAT AND CMAKE_SYSTEM_PROCESSOR MATCHES "64$")
-    set(LIB_SUFFIX 64)
-endif()
 set(LIB_SUFFIX ${LIB_SUFFIX} CACHE STRING "lib directory suffix")
